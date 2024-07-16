8d52cdfc1fc80c1c564b4b60980561212b7f6269
vikasgrover2 <vikasgrover@gmail.com>
2024-07-16 11:03:10 -0400

    Adding perseus email step

--- dags/redshift_dag.py
+++ dags/redshift_dag.py
@@ -217,9 +217,10 @@ end_step10 = EmptyOperator(task_id="end_step10", dag=etl_dag)
 end_step9 >> step10
 step10>>[step10_retention_extract,step10_course_registration_extract]>>end_step10
 
-step10f = PythonOperator(task_id='send_email_perseus',python_callable= email_job, op_kwargs={"module_name":"Workday Perseus","client_name":"Suffolk","recipients":"etlsupp@heliocampus.com"}, dag=etl_dag)
+step10e = PythonOperator(task_id='counts_perseus',python_callable= insert_counts,op_kwargs={"module_name":"Workday Perseus","schemaname": counts_schema,"conn_id":Variable.get("conn_etl")}, dag=etl_dag)
+step10f = PythonOperator(task_id='send_email_perseus',python_callable= email_job,op_kwargs={"module_name":"Workday Hercules","client_name":"Suffolk","recipients":"etlsupp@heliocampus.com","conn_id":Variable.get("conn_etl"), "schemaname": counts_schema}, dag=etl_dag)
 
-end_step10>> step10f
+end_step10>>step10e>> step10f
 
 etl_stop = EmptyOperator(task_id="etl_stop", dag=etl_dag)
 step10f >> etl_stop
